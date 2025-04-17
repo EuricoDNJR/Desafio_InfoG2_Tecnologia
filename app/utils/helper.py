8683cbc0a5858f8b0ca162cb7c62c2a4853f5101
EuricoDNJR <massaultyplay@gmail.com>
2025-04-16 22:53:41 -0300

    feat: Add Firebase initialization and logging setup in helper module

---
+++ app/utils/helper.py
@@ -0,0 +1,36 @@
+import logging
+import datetime
+
+from pytz import timezone
+
+from .firebase_connector import initialize_firebase
+
+
+defined_timezone = timezone("America/Sao_Paulo")
+
+
+# Configurando o logging para usar o timezone definido
+class CustomFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        dt = datetime.datetime.fromtimestamp(record.created)
+        dt = dt.astimezone(defined_timezone)
+        if datefmt:
+            return dt.strftime(datefmt)
+        else:
+            return dt.isoformat()
+
+
+logging.basicConfig(level=logging.INFO)
+
+# Obtendo o logger root
+logger = logging.getLogger()
+
+# Configurando o formatter no handler padrão
+for handler in logger.handlers:
+    handler.setFormatter(
+        CustomFormatter(fmt="%(asctime)s - %(levelname)s - %(message)s")
+    )
+
+logging.info("Starting Firebase connection")
+firebase = initialize_firebase()
+logging.info("Firebase connection started")
