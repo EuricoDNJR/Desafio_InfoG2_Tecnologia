2b59751c3404720119cd7796742625321c84cf69
EuricoDNJR <massaultyplay@gmail.com>
2025-04-16 22:46:12 -0300

    feat: Adjusts main.py lifespan and add logging

--- app/main.py
+++ app/main.py
@@ -1,19 +1,81 @@
+import os
+from contextlib import asynccontextmanager
+
+import dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.db.database import engine
 from app.db.base import Base
-from app.models import user
+from app.db.models import user
+from .routers.auth import auth
+from .utils.helper import logging
 
 
+dotenv.load_dotenv()
+
+ENV = os.getenv("ENV")
+TEST = os.getenv("TEST")
+logging.info(f"ENV: {ENV}")
+logging.info(f"TEST: {TEST}")
+
+api_version = "v0.1.0"
+route_version = "v1"
+
+api_metadata = {
+    "title": "Lu Estilo API",
+    "description": "API de gerenciamento da Lu Estilo",
+    "version": api_version,
+}
+
+if ENV == "dev":
+    app = FastAPI(
+        title=api_metadata["title"],
+        description=api_metadata["description"],
+        version=api_metadata["version"],
+    )
+else:
+    app = FastAPI(
+        title=api_metadata["title"],
+        description=api_metadata["description"],
+        version=api_metadata["version"],
+        # docs_url=None,
+        # redoc_url=None,
+    )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
+)
+
+response_404 = {404: {"description": "Not found"}}
+
+app.include_router(
+    auth.router,
+    prefix=f"/auth",
+    tags=["auth"],
+    responses=response_404,
+)
+
+
+@asynccontextmanager
 async def lifespan(app: FastAPI):
-    print(">> Criando tabelas")
+    logging.info("Starting database connection")
     Base.metadata.create_all(bind=engine)
 
     yield
 
+    logging.info("Closing database connection")
+    engine.dispose()
+
     pass
 
 
-app = FastAPI(lifespan=lifespan)
+app.router.lifespan_context = lifespan
 
 
 @app.get("/")
