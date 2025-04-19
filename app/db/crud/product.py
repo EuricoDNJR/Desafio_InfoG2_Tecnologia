1c4267bedf5b45f9f8c6011a4696d2939fa59659
EuricoDNJR <massaultyplay@gmail.com>
2025-04-19 14:11:08 -0300

    feat: Add get_products function to get products with filter options

--- app/db/crud/product.py
+++ app/db/crud/product.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session
 from app.db.models.product import Product
 
@@ -28,3 +30,28 @@ def create_product(
     db.commit()
     db.refresh(new_product)
     return new_product
+
+
+def get_products(
+    db: Session,
+    skip: int = 0,
+    limit: int = 10,
+    section: Optional[str] = None,
+    min_price: Optional[float] = None,
+    max_price: Optional[float] = None,
+    available: Optional[bool] = None,
+):
+    query = db.query(Product)
+
+    if section:
+        query = query.filter(Product.section.ilike(f"%{section}%"))
+    if min_price is not None:
+        query = query.filter(Product.price >= min_price)
+    if max_price is not None:
+        query = query.filter(Product.price <= max_price)
+    if available is True:
+        query = query.filter(Product.stock > 0)
+    elif available is False:
+        query = query.filter(Product.stock <= 0)
+
+    return query.offset(skip).limit(limit).all()
