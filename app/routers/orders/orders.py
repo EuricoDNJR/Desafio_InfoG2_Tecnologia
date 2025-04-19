c91b602d0b9f5b7cc30d597272ba5750cfb42a66
EuricoDNJR <massaultyplay@gmail.com>
2025-04-19 18:01:34 -0300

    feat: Add list_orders function with filter and pagination

--- app/routers/orders/orders.py
+++ app/routers/orders/orders.py
@@ -1,7 +1,7 @@
 import os
 from decimal import Decimal
 from typing import Optional
-
+from datetime import datetime
 import dotenv
 from fastapi import APIRouter, Depends, Header, HTTPException, Query, status
 from fastapi.responses import JSONResponse
@@ -12,6 +12,7 @@ from ...dependencies import get_db, get_token_header
 from ...db.schemas.orders import (
     OrderCreateRequest,
     OrderResponse,
+    PaginatedOrderResponse,
 )
 from ...utils.helper import firebase, logging
 from ...db.crud import order as crud
@@ -63,3 +64,58 @@ def create_order(order_data: OrderCreateRequest, db: Session = Depends(get_db)):
     except Exception as e:
         logging.error(f"Erro ao criar pedido: {e}", exc_info=True)
         raise HTTPException(status_code=400, detail="Erro ao criar pedido")
+
+
+@router.get(
+    "/orders",
+    response_model=PaginatedOrderResponse,
+    dependencies=[Depends(get_token_header)],
+)
+def list_orders(
+    db: Session = Depends(get_db),
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1),
+    start_date: Optional[datetime] = Query(None),
+    end_date: Optional[datetime] = Query(None),
+    section: Optional[str] = Query(None),
+    order_id: Optional[int] = Query(None),
+    status: Optional[str] = Query(None),
+    client_id: Optional[int] = Query(None),
+):
+    """
+    List orders with pagination and filters.
+
+    E.g:
+
+            {
+                "page": 1,
+                "limit": 10,
+                "start_date": "2023-01-01T00:00:00",
+                "end_date": "2023-12-31T23:59:59",
+                "section": "A",
+                "order_id": 1,
+                "status": "pendente",
+                "client_id": 1,
+            }
+
+    """
+    try:
+        skip = (page - 1) * limit
+        orders = crud.get_orders(
+            db=db,
+            skip=skip,
+            limit=limit,
+            start_date=start_date,
+            end_date=end_date,
+            section=section,
+            order_id=order_id,
+            status=status,
+            client_id=client_id,
+        )
+
+        return {"page": page, "limit": limit, "orders": orders}
+
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Erro ao listar pedidos")
