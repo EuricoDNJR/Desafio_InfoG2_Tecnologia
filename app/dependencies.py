66e3079f6047237eb5ef6025a4c3072fd11460ff
EuricoDNJR <massaultyplay@gmail.com>
2025-04-20 15:21:41 -0300

    feat: Add get_current_user_with_role function to get user by role from JWT token

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,12 +1,12 @@
 import os
 
 import dotenv
-from fastapi import Header, HTTPException, status
+from fastapi import Header, HTTPException, status, Depends
 from firebase_admin import auth
 from sqlalchemy.orm import Session
 
 from app.db.database import SessionLocal
-
+from app.db.crud.user import get_user_by_firebase_id
 
 dotenv.load_dotenv()
 
@@ -43,3 +43,26 @@ async def get_token_header(jwt_token: str = Header()):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid JWT token"
         )
+
+
+def get_current_user_with_role(required_role: str):
+    async def _get_user(jwt_token: str = Header(...), db: Session = Depends(get_db)):
+        try:
+            decoded_token = auth.verify_id_token(jwt_token)
+            uid = decoded_token["uid"]
+
+            # Recupera o usuário do banco com o firebaseId
+            user = get_user_by_firebase_id(db, firebaseId=uid)
+            if not user:
+                raise HTTPException(status_code=401, detail="Usuário não encontrado")
+
+            # Verifica a role do usuário
+            if user.role != required_role:
+                raise HTTPException(status_code=403, detail="Permissão negada")
+
+            return user
+
+        except Exception as e:
+            raise HTTPException(status_code=401, detail=f"Token inválido: {e}")
+
+    return _get_user
